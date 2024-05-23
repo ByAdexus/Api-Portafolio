f28d6d8836329cc80a3cca26ee5aedabe1446518
thisisjose <josecinuwu@gmail.com>
2024-05-23 11:14:04 -0700

    Api avance

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
-import { ConfigurableModuleBuilder, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule.forRoot({envFilePath: 'env', isGlobal:true}), MongooseModule.forRoot(process.env.DB_URI)],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DB_URI, {
+      connectionName: 'cats',
+    })],
+
   controllers: [AppController],
   providers: [AppService],
 })
